0b9c8a06a3e6be0e858a9eb6546bce0101e28047
Pim <pimhoreman@gmail.com>
2021-10-21 16:31:43 +0200

    Simple NPC Behaviour
    -Added Code to HumanRightsDefender.cs

--- GuidingJourney/Assets/Scripts/NPC/HumanRightsDefenderNPC.cs
+++ GuidingJourney/Assets/Scripts/NPC/HumanRightsDefenderNPC.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class HumanRightsDefenderNPC : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform targetObject = null;
+
+    [SerializeField] private float minRange = 6f;
+
+    private int movementSpeed = 0;
+    public int mSpeed = 4;
+
+    private int turnSpeed = 0;
+    public int tSpeed = 3;
+
+
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Vector3.Distance(targetObject.position, transform.position) > minRange)
+        {
+            InitializeMovement();
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(targetObject.position - transform.position), turnSpeed * Time.deltaTime);
+            transform.position += transform.forward * movementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            DisableMovement();
+            return;
+        }
+    }
+
+    private void InitializeMovement()
+    {
+        movementSpeed = mSpeed;
+        turnSpeed = tSpeed;
+    }
+
+    private void DisableMovement()
+    {
+        movementSpeed = 0;
+        turnSpeed = 0;
     }
 }
