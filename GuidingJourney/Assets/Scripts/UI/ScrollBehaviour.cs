bc4c63e1ccdcb107a133a1f3af0ce4d6fb91a960
Pim <pimhoreman@gmail.com>
2021-12-07 14:53:06 +0100

    First art for pbi purse

--- GuidingJourney/Assets/Scripts/UI/ScrollBehaviour.cs
+++ GuidingJourney/Assets/Scripts/UI/ScrollBehaviour.cs
@@ -7,16 +7,31 @@ using UnityEngine.UI;
 public class ScrollBehaviour : MonoBehaviour
 {
     public static Action<int> OnAnimalChangeEvent;
-
+    [SerializeField] private Sprite scrollClosedImage, scrollOpenImage;
     [SerializeField] private GameObject scroll;
+    
     private bool isScrollActive = false;
-   
+    private Image scrollImage;
+
+    private void Awake()
+    {
+        scrollImage = GetComponent<Image>();    
+    }
+
     //Enables and Disables the scroll(image) when the button is clicked
     public void OpenScroll()
     {
         isScrollActive = !isScrollActive;
         scroll.SetActive(isScrollActive);
 
+        if(isScrollActive)
+        {
+            scrollImage.sprite = scrollOpenImage;
+        }
+        else
+        {
+            scrollImage.sprite = scrollClosedImage;
+        }
         //Needs animation in the future
     }
 
