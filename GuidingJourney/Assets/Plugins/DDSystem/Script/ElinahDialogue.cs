d372401d7ae090c0d64bdc07f65b8d87bbb8cccb
Nowi <nowi.lankheet@student.hku.nl>
2022-01-20 16:33:06 +0100

    Auto stash before merge of "Fate" and "origin/Fate"

--- GuidingJourney/Assets/Plugins/DDSystem/Script/ElinahDialogue.cs
+++ GuidingJourney/Assets/Plugins/DDSystem/Script/ElinahDialogue.cs
@@ -15,80 +15,136 @@ public class ElinahDialogue : MonoBehaviour
         //INTRO CUTSCENE
 
         dialogTexts.Add(new DialogData("Today is the day of my big journey.", "Elinah"));
-        dialogTexts.Add(new DialogData("The Peace Brigades International organization said it’d be very risky to go to the event, but this is too important.", "Elinah"));
-        dialogTexts.Add(new DialogData("I have to get there for everyone who’s counting on me.", "Elinah"));
+        dialogTexts.Add(new DialogData("The Peace Brigades International organization said itï¿½d be very risky to go to the event, but this is too important.", "Elinah"));
+        dialogTexts.Add(new DialogData("I have to get there for everyone whoï¿½s counting on me.", "Elinah"));
         dialogTexts.Add(new DialogData("The organization agreed with me, on the condition that they can send a guide. Protection so that nothing bad can happen to me on my way to the event.", "Elinah"));
-        dialogTexts.Add(new DialogData("They told me a guiding figure could help me. But I don’t know what exactly they meant with that…", "Elinah"));
+        dialogTexts.Add(new DialogData("They told me a guiding figure could help me. But I donï¿½t know what exactly they meant with thatï¿½", "Elinah"));
 
         dialogTexts.Add(new DialogData("Ah, this letter is from the Peace Brigades organization.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("There’s a fox stamp on it and wow, it looks kind of magical!", "Elinah"));
+        dialogTexts.Add(new DialogData("Thereï¿½s a fox stamp on it and wow, it looks kind of magical!", "Elinah"));
 
-        dialogTexts.Add(new DialogData("You must be the guide I was told about. Well, Fox, if you’re supposed to guide me on my journey, then let’s begin this adventure.", "Elinah"));
+        dialogTexts.Add(new DialogData("You must be the guide I was told about. Well, Fox, if youï¿½re supposed to guide me on my journey, then letï¿½s begin this adventure.", "Elinah"));
 
         dialogTexts.Add(new DialogData("Are you ready?", "Elinah"));
+
 
         //Puzzle One
 
         dialogTexts.Add(new DialogData("Come on, fox, follow me!", "Elinah"));
 
-        dialogTexts.Add(new DialogData("Today’s event is very important, people from around the whole region will show up.", "Elinah"));
+        dialogTexts.Add(new DialogData("Todayï¿½s event is very important, people from around the whole region will show up.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("So come on, let’s get going!", "Elinah"));
+        dialogTexts.Add(new DialogData("So come on, letï¿½s get going!", "Elinah"));
 
-        dialogTexts.Add(new DialogData("I lost something last night. It’s somewhat important for today, so we should make a quick stop before we can continue.", "Elinah"));
+        dialogTexts.Add(new DialogData("I lost something last night. Itï¿½s somewhat important for today, so we should make a quick stop before we can continue.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("Let’s stay on the path, alright?", "Elinah"));
+        dialogTexts.Add(new DialogData("Letï¿½s stay on the path, alright?", "Elinah"));
 
-        dialogTexts.Add(new DialogData("Come on, we shouldn’t take too long or we’ll be late.", "Elinah"));
+        dialogTexts.Add(new DialogData("Come on, we shouldnï¿½t take too long or weï¿½ll be late.", "Elinah"));
+
 
         //Puzzle Two
 
-        dialogTexts.Add(new DialogData("After I dropped my bag in the dark yesterday, I thought I saw another fox take it into this tunnel. I don’t think I could reach it, but maybe you can?", "Elinah"));
+        dialogTexts.Add(new DialogData("After I dropped my bag yesterday evening, I thought I saw an animal take it into this tunnel. I donï¿½t think I could reach it, but maybe you can?", "Elinah"));
 
         dialogTexts.Add(new DialogData("If you get close to that little hole, you could probably dig your way in!", "Elinah"));
 
-        dialogTexts.Add(new DialogData("Yes, that’s my bag! Thank you for retrieving it!", "Elinah"));
+        dialogTexts.Add(new DialogData("Yes, thatï¿½s my bag! Thank you for retrieving it!", "Elinah"));
 
-        dialogTexts.Add(new DialogData("I needed this bag, because there’s a lot of stuff in it. I brought some flyers and banners for the protest we’re heading to, but also things like make-up, because I do want to look good!", "Elinah"));
+        dialogTexts.Add(new DialogData("I needed this bag, because thereï¿½s a lot of stuff in it. I brought some flyers and banners for the protest weï¿½re heading to, but also things like make-up, because I do want to look good!", "Elinah"));
+
 
         //CUTSCENE MIDWAY
 
+        dialogTexts.Add(new DialogData("Did you see all the chopped wood?", "Elinah"));
+
+        dialogTexts.Add(new DialogData("That forest used to be much larger, but because of illegal wood cutting the forest has been severely damaged.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Protecting forests like these is one of the main topics of our protest today!", "Elinah"));
+
+
         //Puzzle Three
 
-        dialogTexts.Add(new DialogData("The human rights defender should pass this road. Let’s make sure she can’t reach the city!", "Voice1"));
-        dialogTexts.Add(new DialogData("It’s a woman right? Then this should be easy.", "Voice2"));
+        dialogTexts.Add(new DialogData("The human rights defender should be passing this road. Letï¿½s make sure she canï¿½t reach the city!", "Voice1"));
+        
+        dialogTexts.Add(new DialogData("Hold up, itï¿½s a woman? Well, that should make this easy", "Voice2"));
 
-        dialogTexts.Add(new DialogData("Oh no, do you hear that? They must have blocked off the road ahead. The PBI handbook says we should find an alternate escape route, so we don’t put ourselves into a dangerous situation.", "Elinah"));
+        dialogTexts.Add(new DialogData("Oh no, do you hear that? They will try to block me on the road ahead. The PBI handbook says we should find an alternate escape route, so we can avoid a dangerous situation.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("This forest? Well, it’s a pretty dense forest, so it might be difficult for me to navigate. But it does provide an alternative route to the campsite! Great thinking, fox!.", "Elinah"));
+        dialogTexts.Add(new DialogData("This forest? Well, itï¿½s a pretty dense forest, so it might be difficult for me to navigate. But it does provide an alternative route to the campsite! Great thinking, fox!.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("As I thought, I’m not as small as you are, fox. I can’t move through the forest as easily.", "Elinah"));
+        dialogTexts.Add(new DialogData("As I thought, Iï¿½m not as small as you are, fox. I canï¿½t move through the forest as easily.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("You’re much smaller. Perhaps you could crawl your way through these piles?", "Elinah"));
+        dialogTexts.Add(new DialogData("Youï¿½re much smaller. Perhaps you could crawl your way through these piles?", "Elinah"));
 
         dialogTexts.Add(new DialogData("Fox, is there anything you can do on the other side that would allow me to pass as well?", "Elinah"));
 
-        dialogTexts.Add(new DialogData("Wait before you go on, I’m still stuck here!", "Elinah"));
+        dialogTexts.Add(new DialogData("Wait before you go on, Iï¿½m still stuck here!", "Elinah"));
 
-        dialogTexts.Add(new DialogData("It worked! Let’s continue.", "Elinah"));
+        dialogTexts.Add(new DialogData("It worked! Letï¿½s continue.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("We can’t go there, it's too dangerous. We’ll go through the forest.", "Elinah"));
+        dialogTexts.Add(new DialogData("We canï¿½t go there, it's too dangerous. Weï¿½ll go through the forest.", "Elinah"));
 
 
         //Puzzle Four
 
-        dialogTexts.Add(new DialogData("That letter is very important to me, can you see it from that height?", "Elinah"));
+        dialogTexts.Add(new DialogData("That letter is my protest permit, so itï¿½s very important. Can you see it from that height?", "Elinah"));
 
         dialogTexts.Add(new DialogData("Oh, you found it! Please, bring it back to me now.", "Elinah"));
 
-        dialogTexts.Add(new DialogData("Now that we have the letter back, we can go on! There’s no time to waste!", "Elinah"));
+        dialogTexts.Add(new DialogData("Now that we have the protest permission letter back, we can go on! Thereï¿½s no time to waste!", "Elinah"));
 
+        dialogTexts.Add(new DialogData("I have a special letter with me. Itï¿½s a protest permit, which states that I will lead todayï¿½s protest.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("You can run around a little. Iï¿½ll go check out the PBI tent. You can come back to me if youï¿½re ready to continue!", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Great, Iï¿½ll show you the official PBI letter I received this morning.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Another stamp, but with a dove on it? It has the same magical look as the fox one does.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Perhaps I should see what this one does as well?", "Elinah"));
+
+        dialogTexts.Add(new DialogData("I think that with this letter, you can change forms!", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Oh no, thatï¿½s my protest permission letter. I need to have it back before we get to the protest.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Well, if you can fly high, you could easily locate my letter. Could you go find it for me and then bring it back?", "Elinah"));
 
         //CUTSCENE EXTRA
 
+        dialogTexts.Add(new DialogData("We then continued our journey together.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("After a while, we came across an area with a lot of smoke.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Some people probably tried to obstruct me again by throwing smoke bombs that would keep me from seeing where I was going.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("The dove was smart and grabbed my flashlight and flew up high with it to show me the path.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Finally, we arrived at the city. Some people had made a barrier there to keep me from entering the city.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("But when my little guide snuck inside of the city and grabbed a megaphone for me, I was able to speak loud enough so that the other protesters could hear me.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("They all gathered around the blockade in an attempt to help me get through!", "Elinah"));
+
         //CUTSCENE ENDING
 
+        dialogTexts.Add(new DialogData("Ohno, they all came to support the human rights defender. This is a battle we cannot win, letï¿½s run!", "Voice1"));
+
+        dialogTexts.Add(new DialogData("Thanks, everyone. If we all work together and with the help of PBI, they canï¿½t stop us that easily!", "Elinah"));
+
+        dialogTexts.Add(new DialogData("Multinational companies are destroying our environments!! They have cut our trees and polluted our lands more than enough.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("And in the past, if someone stood up against them, those brave people would mysteriously disappear.", "Elinah"));
+
+        dialogTexts.Add(new DialogData("So many of our local heroes have gone missingï¿½", "Elinah"));
+
+        dialogTexts.Add(new DialogData("But together with the Peace Brigades International, I will make sure that we stand strong and we are safe!", "Elinah"));
+
+        //Template LineAdder
+        //dialogTexts.Add(new DialogData("...", "Elinah"));
+
+        //Dialogue end here!!
         DialogManager.Show(dialogTexts);
 
     }
