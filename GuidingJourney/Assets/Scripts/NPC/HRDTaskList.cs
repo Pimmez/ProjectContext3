ee1e9410a146b352992ab48a5616a965f8845232
Pim <pimhoreman@gmail.com>
2022-01-24 12:30:12 +0100

    Added Backpack, Elinah Dialogue Changes

--- GuidingJourney/Assets/Scripts/NPC/HRDTaskList.cs
+++ GuidingJourney/Assets/Scripts/NPC/HRDTaskList.cs
@@ -47,7 +47,7 @@ public class HRDTaskList : GenericSingleton<HRDTaskList, HRDTaskList>
                 completedTaskBag = true;
                 Destroy(targetObject);
                 SoundManager.Instance.Play(sfxClip);
-          
+                GameManager.Instance.isHoldingObject = false;
                 if (completedTaskBag)
                 {
                     dialogueElinah.AfterCaveDialogue();
